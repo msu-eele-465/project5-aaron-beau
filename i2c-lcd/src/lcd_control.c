bc44522566a2a417a1d737e31b382d9292d3218a
bcoburn0618 <beaucoburn2003@gmail.com>
2025-04-01 20:20:11 -0600

    Trying to fix I2C

--- i2c-lcd/src/lcd_control.c
+++ i2c-lcd/src/lcd_control.c
@@ -15,24 +15,7 @@ void LCD_init(){
     P2OUT &= ~(RS | EN);
     P2DIR |= (RS | EN);
 
-     //------------------------------- I2C Initialization -----------------------------    
-//--Put eUSCI_B0 into software reset to allow configuration
-    UCB0CTLW0 |= UCSWRST;       
-
-//--Configure eUSCI_B0 for I2C Slave mode
-    UCB0CTLW0 |= UCMODE_3 | UCSYNC;         // Set I2C mode, synchronous operation
-    UCB0I2COA0 = SLAVE_ADDR | UCOAEN;       // Set slave address & enable
-
-//--Configure Ports for I2C SDA (P1.2) and SCL (P1.3)
-    P1SEL1 &= ~(BIT3 | BIT2);               // Select primary module function for I2C
-    P1SEL0 |= (BIT3 | BIT2);
-
-//--Enable I2C Module
-    UCB0CTLW0 &= ~UCSWRST;                  // Release eUSCI_B0 from reset
-
-//--Enable I2C Interrupts
-    UCB0IE |= UCRXIE0;                      // Enable I2C receive interrupt
-    __bis_SR_register(GIE);                 // Enable global interrupts
+    
 
    
 
